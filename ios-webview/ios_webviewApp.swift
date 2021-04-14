34da571cf337353a4cee6af2b3cc394a66f3f21b
matthew <cheolwoong.lee@bringprice.com>
2021-04-14 11:18:42 +0900

    Initial Commit

---
+++ ios-webview/ios_webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_webviewApp.swift
+//  ios-webview
+//
+//  Created by Matthew on 2021/04/14.
+//
+
+import SwiftUI
+
+@main
+struct ios_webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
